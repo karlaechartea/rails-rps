6628555d4d8ad519865c2d8050b78dd9e8886445
karlaechartea <k.echartea@chicagobooth.edu>
2024-10-30 17:23:41 +0000

    sav

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get("/rock", { :controller => "zebra", :action => "giraffe" })
 
+  get("/paper", { :controller => "zebra", :action => "giraffe" })
+
+  get("/scissors", { :controller => "zebra", :action => "giraffe" })
 end
